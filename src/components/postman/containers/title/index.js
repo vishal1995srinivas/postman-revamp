d5c3d58d6bd383eef9849747cb73e8faa2b4238c
Vishal Srinivas <vishal1995srinivas@gmail.com>
2020-10-29 19:11:07 +0530

    logout route added

--- src/components/postman/containers/title/index.js
+++ src/components/postman/containers/title/index.js
@@ -1,69 +1,67 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Input } from 'semantic-ui-react';
 import { getJwt } from '../../helpers/jwt';
 import { Menu, Dropdown } from 'antd';
 import { DownOutlined } from '@ant-design/icons';
+import { withRouter } from 'react-router-dom';
 import './index.css';
-function Title(props) {
-	const handleTitle = (event) => {
-		props.handleTitle(event);
+
+class Title extends Component {
+	handleTitle = (event) => {
+		this.props.handleTitle(event);
 	};
-	const { title } = props;
-	const jwt = getJwt();
-	let username = jwt.userName;
-	let letter = username.charAt(0).toUpperCase();
-	const menu = (
-		<Menu>
-			<Menu.Item className="dropDownHeader">
-				<a
-					target="_blank"
-					onClick={(e) => {
-						e.preventDefault();
-						localStorage.removeItem('user-Details');
-						props.history.push('/login');
-					}}
-					className="dropdown"
-					rel="noopener noreferrer"
-					href="/login"
-				>
-					Log out
-				</a>
-			</Menu.Item>
-		</Menu>
-	);
-	const logOutHandler = () => {
+	logOutHandler = (e) => {
+		e.preventDefault();
 		localStorage.removeItem('user-Details');
 		this.props.history.push('/login');
 	};
-	return (
-		<div className="urlTitle">
-			<Input
-				transparent
-				fluid
-				inverted
-				icon="tag"
-				iconPosition="left"
-				placeholder="Request Title Here"
-				className="inputUrl"
-				value={title}
-				onChange={handleTitle}
-			/>
-			<div className="loginLetter">
-				<Dropdown overlay={menu}>
+	render() {
+		const { title } = this.props;
+		const { handleTitle, logOutHandler } = this;
+		const jwt = getJwt();
+		let username = jwt.userName;
+		let letter = username.charAt(0).toUpperCase();
+		const menu = (
+			<Menu>
+				<Menu.Item className="dropDownHeader">
 					<a
-						href="#index"
-						className="dropdown"
+						target="_blank"
 						onClick={(e) => {
 							e.preventDefault();
 							localStorage.removeItem('user-Details');
 							this.props.history.push('/login');
 						}}
+						className="dropdown"
+						rel="noopener noreferrer"
+						href="/login"
 					>
-						{letter} <DownOutlined />
+						Log out
 					</a>
-				</Dropdown>
+				</Menu.Item>
+			</Menu>
+		);
+		return (
+			<div className="urlTitle">
+				<Input
+					transparent
+					fluid
+					inverted
+					icon="tag"
+					iconPosition="left"
+					placeholder="Request Title Here"
+					className="inputUrl"
+					value={title}
+					onChange={handleTitle}
+				/>
+				<div className="loginLetter">
+					<Dropdown overlay={menu}>
+						<a href="#index" className="dropdown">
+							{letter} <DownOutlined />
+						</a>
+					</Dropdown>
+				</div>
 			</div>
-		</div>
-	);
+		);
+	}
 }
-export default Title;
+export default withRouter(Title);
